4188195abbdc57086835183c5a0fcf4747670d0e
mengyang0313 <mengyang0313@gmail.com>
2021-10-19 19:17:33 +0800

    init

--- src/components/VueVideoPlayer/index.vue
+++ src/components/VueVideoPlayer/index.vue
@@ -13,7 +13,7 @@ export default {
         return {
             playerOptions: {
                 playbackRates: [0.7, 1.0, 1.5, 2.0], // 可选的播放速度
-                autoplay: false, // 如果为true,浏览器准备好时开始回放
+                autoplay: true, // 如果为true,浏览器准备好时开始回放
                 muted: false, // 默认情况下将会消除任何音频
                 loop: false, // 是否视频一结束就重新开始
                 preload: 'auto', // 建议浏览器在<video>加载元素后是否应该开始下载视频数据;auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
